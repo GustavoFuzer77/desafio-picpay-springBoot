95983314e90bc992e90b1e65cc7a293a7bae4bb7
Gustavo Fuzer <gustavofuzer256@gmail.com>
2024-08-31 19:52:44 -0300

    fix: changing the authorization form to correct the flaw

--- src/main/java/com/picpay/api/services/auth/Authorization.java
+++ src/main/java/com/picpay/api/services/auth/Authorization.java
@@ -2,8 +2,25 @@ package com.picpay.api.services.auth;
 
 public record Authorization() {
 
-  public static boolean isAuthorized(String message) {
-    return message.equals("autorizado");
+  public static boolean isAuthorized(String jsonResponse) {
+    String trimmedResponse = jsonResponse.trim();
+    String message = "";
+
+    if (trimmedResponse.startsWith("{") && trimmedResponse.endsWith("}")) {
+      trimmedResponse = trimmedResponse.substring(1, trimmedResponse.length() - 1);
+      String[] keyValuePair = trimmedResponse.split(":");
+
+      if (keyValuePair.length == 2) {
+        String key = keyValuePair[0].trim().replace("\"", "");
+        String value = keyValuePair[1].trim().replace("\"", "");
+
+        if ("message".equals(key)) {
+          message = value;
+        }
+      }
+    }
+
+    return "autorizado".equals(message);
   }
 
 }
